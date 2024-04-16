e8e1a98941f13cde2841bd362f55e34b2fb356d6
Santiago Medina <santimedinaok.10@gmail.com>
2024-04-16 12:09:41 -0300

    punto 2 b

--- tp3_punto-02.c
+++ tp3_punto-02.c
@@ -7,7 +7,7 @@
 
 //Funcion para mostrar la matriz.
 
-void mostrarMatriz();
+void mostrarMatriz(int matriz[ANIO][MES]);
 
 int main(void){
 
@@ -19,9 +19,24 @@ int main(void){
     {
         for (int j = 0; j < MES; j++)
         {
-            matriz[i][j] = 10000 + rand() % (40001);  
+            matriz[i][j] = 10000 + rand() % (50000 - 10000 + 1);  
             // limite inferior + rand() % (lim-sup - lim-inf + 1)
         }  
+    }
+
+    mostrarMatriz(matriz);
+    
+}
+
+void mostrarMatriz(int matriz[ANIO][MES]){
+
+    for (int i = 0; i < ANIO; i++)
+    {
+        for (int j = 0; j < MES; j++)
+        {
+            printf(" %d ", matriz[i][j]);
+        }  
+        printf("\n");
     }
     
 }
